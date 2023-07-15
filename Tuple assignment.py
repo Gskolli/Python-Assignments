196636f904639b4869fd976f6fbdc353ec426c0b
Guru <139640547+Gskolli@users.noreply.github.com>
2023-07-15 21:30:40 +0530

    Add files via upload

---
+++ Tuple assignment.py
@@ -0,0 +1,52 @@
+ #ASSIGNMENT OF TUPLE ()
+
+# Write a Python program to create a tuple.
+
+########tup1 = 1,2,3,6
+#print(tup1)
+#tup1 = tuple()
+#print(type(tup1))
+
+# Write a Python program to add an element to a tuple.
+
+tup2 = (3,5,4,6)
+print(tup2)     
+#tup2 = tup2 + (7,8)
+#print(tup2)
+
+#using index and +
+
+tup2 = tup2[0:1] + (2,4)
+print(tup2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
